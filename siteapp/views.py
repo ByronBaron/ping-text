e54700176cc5bbff0e16e4116c8493460eb5fe3c
Mike Kartsonakis <mike@Byron.0.2.15>
2014-12-16 02:28:14 +0200

    tenth commit

--- siteapp/views.py
+++ siteapp/views.py
@@ -6,7 +6,10 @@ from django.contrib.auth import authenticate, login, logout
 from subprocess import Popen
 from django.views import generic
 from siteapp.models import Text
+import json
 # Create your views here.
+
+#login view
 def login_user(request):
     logout(request)
     username = password = ''
@@ -25,18 +28,21 @@ def login_user(request):
 def home_page(request):
     return render_to_response('siteapp/home.html')
 
+#ping views
+
 @login_required(login_url='/login/')
 def ping(request):
     return render_to_response('siteapp/ping_page.html', context_instance=RequestContext(request))
 
 @login_required(login_url='/login/')
 def ping_index(request):
-    var = raw_input('Enter you site here: ')
-    p = Popen(["ping", "-c", "7", var])
-    return render_to_response('siteapp/ping_input.html', p)
+    x = [1, 2, 3, 4, 5]
+    json.dumps(x)
+    return render_to_response('siteapp/ping_input.html', x)
 
 #text views
 
 class TextView(generic.ListView): 
     model = Text
     template_name  = "siteapp/text_display.html"
+
